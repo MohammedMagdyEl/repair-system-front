8094c3524ccace0c15c92b44c9fe7545fa8d1fbf
Mohammed Magdy <160265981+MohammedMagdyEl@users.noreply.github.com>
2026-05-10 09:10:17 +0300

    Open add order

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
           path="/add"
           element={
             <PrivateRoute>
-              <LockedPage/>
+              <AddOrder/>
               {/* <AddOrder /> */}
             </PrivateRoute>
           }
